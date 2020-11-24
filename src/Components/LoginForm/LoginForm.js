599b3f06b9d4521ef10382954b779567e812dcbb
JanJaapvG <jan.jaap.v.g@gmail.com>
2020-11-24 19:15:47 +0100

    added a website title and icon

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 export default function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(email,password);
+    
     return (
         <div id = "loginForm">
             <h1>Inloggen</h1>
                 <form>
                     <input type="email" required onChange = {e => setEmail(e.target.value)}></input>
-                    <input type="password" required onChange = {e => setEmail(e.target.value)}></input>
+                    <input type="password" required onChange = {e => setPassword(e.target.value)}></input>
                     <button type="Login">AAN DE SLAG</button>
                 </form>
         </div>
