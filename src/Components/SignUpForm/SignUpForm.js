22a119062a14d1cfc1fe8213854fb7cea2cf092f
JanJaapvG <jan.jaap.v.g@gmail.com>
2020-11-25 11:13:55 +0100

    EDIT: LoginForm CSS. ADDED: Link to Header

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -10,15 +10,16 @@ export default function SignUpForm() {
     }
 
     console.log(email);
+
     return (
-        <div id= "SignUpForm">
+        <div id= "sign-up-form">
             <h1>Gratis series, films en meer kijken?</h1>
             <h2>Is Netflix te duur?</h2>
             <h3>Schrijf je dan nu in en bekijk alles gratis!</h3>
-            <form>
-                <input className = "input-field" type="email" required placeholder = "Vul hier je e-mail adres in" onChange = {e => setEmail(e.target.value)}></input>
-                <button id = "submit-button" className = "primary-button" type="submit" onClick = {submitEmail}>AAN DE SLAG</button>
-            </form>
+                <form>
+                    <input className = "input-field" type="email" required placeholder = "Vul hier je e-mail adres in" onChange = {e => setEmail(e.target.value)}></input>
+                    <button id = "submit-button" className = "primary-button" type="submit" onClick = {submitEmail}>AAN DE SLAG</button>
+                </form>
         </div>
     )
 }
