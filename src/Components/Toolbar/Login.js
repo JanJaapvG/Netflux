954c6d49142aeeb1b5ff8b1d2297401933a8f189
JanJaapvG <jan.jaap.v.g@gmail.com>
2020-11-25 18:44:03 +0100

    CREATED: Redux rootReducer and index in Store. ADDED: Redux packages

--- src/Components/Toolbar/Login.js
+++ src/Components/Toolbar/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+
     return (
         <div id="login-button">
             <Link to='/login'>
